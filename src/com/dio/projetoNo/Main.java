89c2e0c49bc15b3842da29c69e8f38a1cd6e996b
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2021-12-08 12:43:03 -0400

    Criação da class Main e da class No

---
+++ src/com/dio/projetoNo/Main.java
@@ -0,0 +1,28 @@
+package com.dio.projetoNo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		No no1 = new No("no1");
+		No no2 = new No("no2");
+		No no3 = new No("no3");
+		No no4 = new No("no4");
+		no1.setProximoNo(no2);
+		no2.setProximoNo(no3);
+		no3.setProximoNo(no4);
+
+		/* no1->no2->no3->no4->null */
+
+		System.out.println(no1);
+		System.out.println(no1.getProximoNo());
+		System.out.println(no2);
+		System.out.println("-----------------------");
+		System.out.println(no1);
+		System.out.println(no1.getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
+	}
+
+}
